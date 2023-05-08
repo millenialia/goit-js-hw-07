986e78b67de65de6a8dd5d24206ef9c51f3816c7
millenialia <lenalebo@gmail.com>
2023-05-08 22:40:27 +0200

    HW done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const makeGalleryMarkup = ({ preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" /></a></li>`
+
+const galleryEl = document.querySelector('.gallery')
+
+
+const makeGalleryTable = galleryItems
+    .map(makeGalleryMarkup)
+    .join('')
+
+galleryEl.insertAdjacentHTML('afterbegin', makeGalleryTable)
+    
+galleryEl.addEventListener('click', openPicture)
+
+
+function openPicture(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    const currentPicture = event.target.dataset.source
+    const instance = basicLightbox.create(`
+    <img src=${currentPicture}>
+    `)
+    instance.show()
+
+    window.addEventListener("keydown", ({ code }) => {
+    
+        if (code === "Escape") {
+            instance.close()
+        }
+    })
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const makeGalleryMarkup = ({ preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+
+const galleryEl = document.querySelector('.gallery')
+
+const makeGalleryTable = galleryItems
+    .map(makeGalleryMarkup)
+    .join('')
+
+galleryEl.insertAdjacentHTML('afterbegin', makeGalleryTable)
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+    
